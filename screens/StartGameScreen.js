07bc01bb42377d418eea9edb06c99b9305ea1101
Wen Xuan <leongwenxuan0@gmail.com>
2019-10-20 13:14:15 +0800

    card look styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -28,7 +28,18 @@ const styles = StyleSheet.create({
         marginVertical: 10,
     },
     inputContainer: {
-
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 5,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
     },
     buttonContainer: {
         flexDirection: 'row',

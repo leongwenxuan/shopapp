2a14996fdfd0164eb15ed631e08434c9706e3008
Wen Xuan <leongwenxuan0@gmail.com>
2019-10-20 14:13:21 +0800

    keyboard dismiss enabled

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View , Text, StyleSheet, Button } from 'react-native';
+import { View , Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard} from 'react-native';
 
 import Card from '../components/card';
 import Input from '../components/input';
@@ -14,6 +14,9 @@ const StartGameScreen = props => {
     };
 
     return (
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
         <View style={styles.screen}>
             <Text style={styles.title}> Start A New Game!</Text>
                 <Card style={styles.inputContainer}>
@@ -25,6 +28,7 @@ const StartGameScreen = props => {
                 </View>
                 </Card>
         </View>
+        </TouchableWithoutFeedback>
     );
 };
 
